f0ccda790cf7ece90acf1dc9e7bf6a310480112b
glcordon <glcordon@gmail.com>
2020-07-27 21:54:18 -0400

    fixed modal and avatar on show page

--- resources/views/clients/show.blade.php
+++ resources/views/clients/show.blade.php
@@ -387,11 +387,9 @@
 @endpush
 
 @endsection
+
+
 @section('scripts')
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.9.0/moment.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.37/js/bootstrap-datetimepicker.min.js"></script>  
-
-
 @parent
 <script>
     $(function () {
